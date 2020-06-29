99e6af8780b7440b7e84ebfe0ae3db095fe8f26a
lucky <>
2020-06-29 22:15:41 +0300

    changed sockets names flag to plist path flag removed some proxy logging when error is valid

--- src/main.go
+++ src/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"os"
 	"strings"
 	"time"
@@ -14,65 +14,114 @@ import (
 )
 
 const (
-	Version = "0.0.1"
+	Version = "0.1.0"
 
-	FlagNames       = "n"
+	FlagPlist       = "p"
 	FlagDestination = "d"
 	FlagTimeout     = "t"
 	FlagBufferSize  = "b"
 
-	TCP  = "tcp"
-	UDP  = "udp"
-	TCP6 = TCP + "6"
-	UDP6 = UDP + "6"
-
 	ExitSuccess = 0
 	ExitUsage   = 2
+
+	DefaultPlist = "/Library/LaunchDaemons/me.lucky.launch-proxy.plist"
 )
 
-func start(name string, noHost bool, opts *Opts) error {
+func start(name string, opts *Opts) error {
 	fds, err := launch.ActivateSocket(name)
 	if err != nil {
 		return err
 	}
-	dest := opts.dest
-	if noHost {
-		switch name {
-		case TCP6, UDP6:
-			dest = "[::1]" + dest
-		default:
-			dest = "127.0.0.1" + dest
-		}
-	}
-	name = name[:min(3, len(name))]
+	name = strings.ToLower(name)
+	isStream := strings.HasPrefix(name, proxy.TCP)
+	isPacket := strings.HasPrefix(name, proxy.UDP)
+	var prx proxy.Proxy
 	for _, fd := range fds {
-		switch name {
-		case TCP:
-			_, err = proxy.StartStream(fd, dest, opts.timeout)
-		case UDP:
-			_, err = proxy.StartPacket(fd, dest, opts.timeout, opts.bufSize)
+		switch {
+		case isStream:
+			prx, err = proxy.NewFileStream(fd, opts.dest, opts.timeout)
+		case isPacket:
+			prx, err = proxy.NewFilePacket(
+				fd,
+				opts.dest,
+				opts.timeout,
+				opts.bufSize,
+			)
 		default:
 			return fmt.Errorf("Invalid name: %q", name)
 		}
 		if err != nil {
 			return err
 		}
+		go func(prx proxy.Proxy) {
+			prx.Start()
+			<-prx.WaitChan()
+			log.Fatalln("Proxy exited")
+		}(prx)
 	}
 	return nil
 }
 
-func min(v ...int) int {
-	res := v[0]
-	for _, i := range v[1:] {
-		if i < res {
-			res = i
+func parsePlist(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	dec := xml.NewDecoder(file)
+	const (
+		Key   = "key"
+		Array = "array"
+		Dict  = "dict"
+	)
+	depth := -1
+	found := false
+	res := []string{}
+Loop:
+	for {
+		token, err := dec.Token()
+		if err != nil {
+			return nil, err
+		}
+		switch element := token.(type) {
+		case xml.StartElement:
+			switch element.Name.Local {
+			case Key:
+				if (!found && depth != 0) || (found && depth != 1) {
+					continue
+				}
+				var key string
+				if err = dec.DecodeElement(&key, &element); err != nil {
+					log.Println(err)
+					continue
+				}
+				if !found {
+					if key == "Sockets" {
+						found = true
+					}
+				} else {
+					res = append(res, key)
+				}
+			case Array, Dict:
+				depth++
+			}
+		case xml.EndElement:
+			switch element.Name.Local {
+			case Array:
+				depth--
+			case Dict:
+				depth--
+				if found && depth == 0 {
+					break Loop
+				}
+			}
 		}
 	}
-	return res
+	return res, nil
 }
 
 type Opts struct {
-	names   []string
+	plist   string
 	dest    string
 	timeout time.Duration
 	bufSize int
@@ -82,11 +131,7 @@ func getOpts() *Opts {
 	opts := &Opts{}
 	isHelp := flag.Bool("h", false, "Print help and exit")
 	isVersion := flag.Bool("V", false, "Print version and exit")
-	names := flag.String(
-		FlagNames,
-		TCP+","+UDP,
-		fmt.Sprintf("Comma separated socket names { %s | %s }", TCP, UDP),
-	)
+	flag.StringVar(&opts.plist, FlagPlist, DefaultPlist, "Path to plist file")
 	flag.StringVar(&opts.dest, FlagDestination, "", "Destination address")
 	flag.DurationVar(
 		&opts.timeout,
@@ -109,7 +154,6 @@ func getOpts() *Opts {
 		fmt.Println(Version)
 		os.Exit(ExitSuccess)
 	}
-	opts.names = strings.Split(*names, ",")
 	if opts.dest == "" {
 		os.Stderr.WriteString("Empty destination\n")
 		os.Exit(ExitUsage)
@@ -119,12 +163,16 @@ func getOpts() *Opts {
 
 func main() {
 	opts := getOpts()
-	noHost := false
-	if host, _, err := net.SplitHostPort(opts.dest); err == nil && host == "" {
-		noHost = true
+	names, err := parsePlist(opts.plist)
+	if err != nil {
+		log.Fatalln(err)
 	}
-	for _, name := range opts.names {
-		if err := start(strings.TrimSpace(name), noHost, opts); err != nil {
+	if len(names) == 0 {
+		log.Fatalln("Socket names not found in", opts.plist)
+	}
+	log.Println("Found socket names:", names)
+	for _, name := range names {
+		if err := start(name, opts); err != nil {
 			log.Fatalln(err)
 		}
 	}
--- src/proxy/proxy.go
+++ src/proxy/proxy.go
@@ -13,11 +13,18 @@ import (
 )
 
 const (
+	TCP = "tcp"
+	UDP = "udp"
+
 	DefaultTimeout    = 1 << 3 * time.Second
 	DefaultBufferSize = 1 << 9
 )
 
-func StartStream(fd int, dest string, timeout time.Duration) (*Stream, error) {
+func NewFileStream(
+	fd int,
+	dest string,
+	timeout time.Duration,
+) (*Stream, error) {
 	file, err := newFile(fd)
 	if err != nil {
 		return nil, err
@@ -27,17 +34,10 @@ func StartStream(fd int, dest string, timeout time.Duration) (*Stream, error) {
 	if err != nil {
 		return nil, err
 	}
-	if timeout < 1 {
-		timeout = DefaultTimeout
-	}
-	addr := ln.Addr()
-	stream := NewStream(ln, dest, timeout)
-	log.Printf("[%s] %s -> %q\n", addr.Network(), addr, dest)
-	go stream.Start()
-	return stream, nil
+	return NewStream(ln, dest, timeout), nil
 }
 
-func StartPacket(
+func NewFilePacket(
 	fd int,
 	dest string,
 	timeout time.Duration,
@@ -52,61 +52,96 @@ func StartPacket(
 	if err != nil {
 		return nil, err
 	}
-	if timeout < 1 {
-		timeout = DefaultTimeout
-	}
-	if bufSize < 1 {
-		bufSize = DefaultBufferSize
-	}
-	addr := pc.LocalAddr()
-	packet := NewPacket(pc, dest, timeout, bufSize)
-	log.Printf("[%s] %s -> %q\n", addr.Network(), addr, dest)
-	go packet.Start()
-	return packet, nil
+	return NewPacket(pc, dest, timeout, bufSize), nil
 }
 
 func newFile(fd int) (*os.File, error) {
-	file := os.NewFile(uintptr(fd), "")
-	if file == nil {
-		return nil, errors.New("Invalid file descriptor: " + strconv.Itoa(fd))
+	if file := os.NewFile(uintptr(fd), ""); file != nil {
+		return file, nil
 	}
-	return file, nil
+	return nil, errors.New("Invalid file descriptor: " + strconv.Itoa(fd))
 }
 
-func splitDest(dest string) (network, address string) {
+func prepareDestination(dest string, addr net.Addr) (network, address string) {
 	substr := "://"
-	idx := strings.Index(dest, substr)
-	if idx == -1 {
+	if idx := strings.Index(dest, substr); idx == -1 {
 		address = dest
 	} else {
 		network = dest[:idx]
 		address = dest[idx+len(substr):]
 	}
+	if network == "" {
+		network = addr.Network()
+	}
+	if !strings.HasPrefix(network, TCP) &&
+		!strings.HasPrefix(network, UDP) {
+
+		return
+	}
+	if host, _, err := net.SplitHostPort(address); err == nil && host == "" {
+		var ip net.IP
+		switch laddr := addr.(type) {
+		case *net.TCPAddr:
+			ip = laddr.IP
+		case *net.UDPAddr:
+			ip = laddr.IP
+		default:
+			return
+		}
+		if ip.To4() != nil {
+			address = "127.0.0.1" + address
+		} else {
+			address = "[::1]" + address
+		}
+	}
 	return
 }
 
+type Proxy interface {
+	Start()
+	Close()
+	WaitChan() <-chan struct{}
+}
+
 func NewStream(ln net.Listener, dest string, timeout time.Duration) *Stream {
-	network, address := splitDest(dest)
-	if network == "" {
-		network = ln.Addr().Network()
+	network, address := prepareDestination(dest, ln.Addr())
+	if timeout < 1 {
+		timeout = DefaultTimeout
 	}
 	return &Stream{
-		ln:      ln,
-		network: network,
-		address: address,
-		timeout: timeout,
+		ln:       ln,
+		network:  network,
+		address:  address,
+		timeout:  timeout,
+		waitChan: make(chan struct{}),
 	}
 }
 
 type Stream struct {
-	ln      net.Listener
-	network string
-	address string
-	timeout time.Duration
+	ln       net.Listener
+	network  string
+	address  string
+	timeout  time.Duration
+	waitChan chan struct{}
 }
 
 func (p *Stream) Start() {
+	go p.start()
+}
+
+func (p *Stream) Close() {
+	p.ln.Close()
+}
+
+func (p *Stream) WaitChan() <-chan struct{} {
+	return p.waitChan
+}
+
+func (p *Stream) start() {
 	defer p.ln.Close()
+	addr := p.ln.Addr()
+	network := addr.Network()
+	log.Printf("[%s] %s -> [%s] %s\n", network, addr, p.network, p.address)
 	for {
 		conn, err := p.ln.Accept()
 		if err != nil {
@@ -115,8 +150,8 @@ func (p *Stream) Start() {
 		}
 		go p.proxy(conn)
 	}
-	addr := p.ln.Addr()
-	log.Printf("[%s] %s -> exit\n", addr.Network(), addr)
+	log.Printf("[%s] %s -> exit\n", network, addr)
+	close(p.waitChan)
 }
 
 func (p *Stream) proxy(in net.Conn) {
@@ -143,32 +178,32 @@ func (p *Stream) proxy(in net.Conn) {
 	}
 }
 
-func makePacketMap() packetMap {
-	return packetMap{m: make(map[string]net.Conn)}
+func makeConnMap() connMap {
+	return connMap{m: make(map[string]net.Conn)}
 }
 
-type packetMap struct {
-	rw sync.RWMutex
+type connMap struct {
+	mu sync.RWMutex
 	m  map[string]net.Conn
 }
 
-func (m *packetMap) Load(key string) (value net.Conn, ok bool) {
-	m.rw.RLock()
+func (m *connMap) Load(key string) (value net.Conn, ok bool) {
+	m.mu.RLock()
 	value, ok = m.m[key]
-	m.rw.RUnlock()
+	m.mu.RUnlock()
 	return
 }
 
-func (m *packetMap) Store(key string, value net.Conn) {
-	m.rw.Lock()
+func (m *connMap) Store(key string, value net.Conn) {
+	m.mu.Lock()
 	m.m[key] = value
-	m.rw.Unlock()
+	m.mu.Unlock()
 }
 
-func (m *packetMap) Delete(key string) {
-	m.rw.Lock()
+func (m *connMap) Delete(key string) {
+	m.mu.Lock()
 	delete(m.m, key)
-	m.rw.Unlock()
+	m.mu.Unlock()
 }
 
 func NewPacket(
@@ -177,31 +212,51 @@ func NewPacket(
 	timeout time.Duration,
 	bufSize int,
 ) *Packet {
-	network, address := splitDest(dest)
-	if network == "" {
-		network = pc.LocalAddr().Network()
+	network, address := prepareDestination(dest, pc.LocalAddr())
+	if timeout < 1 {
+		timeout = DefaultTimeout
+	}
+	if bufSize < 1 {
+		bufSize = DefaultBufferSize
 	}
 	return &Packet{
-		pc:      pc,
-		storage: makePacketMap(),
-		network: network,
-		address: address,
-		timeout: timeout,
-		bufSize: bufSize,
+		pc:       pc,
+		storage:  makeConnMap(),
+		network:  network,
+		address:  address,
+		timeout:  timeout,
+		bufSize:  bufSize,
+		waitChan: make(chan struct{}),
 	}
 }
 
 type Packet struct {
-	pc      net.PacketConn
-	storage packetMap
-	network string
-	address string
-	timeout time.Duration
-	bufSize int
+	pc       net.PacketConn
+	storage  connMap
+	network  string
+	address  string
+	timeout  time.Duration
+	bufSize  int
+	waitChan chan struct{}
 }
 
 func (p *Packet) Start() {
+	go p.start()
+}
+
+func (p *Packet) Close() {
+	p.pc.Close()
+}
+
+func (p *Packet) WaitChan() <-chan struct{} {
+	return p.waitChan
+}
+
+func (p *Packet) start() {
 	defer p.pc.Close()
+	addr := p.pc.LocalAddr()
+	network := addr.Network()
+	log.Printf("[%s] %s -> [%s] %s\n", network, addr, p.network, p.address)
 	for {
 		buf := make([]byte, p.bufSize)
 		n, addr, err := p.pc.ReadFrom(buf)
@@ -213,8 +268,8 @@ func (p *Packet) Start() {
 			break
 		}
 	}
-	addr := p.pc.LocalAddr()
-	log.Printf("[%s] %s -> exit\n", addr.Network(), addr)
+	log.Printf("[%s] %s -> exit\n", network, addr)
+	close(p.waitChan)
 }
 
 func (p *Packet) nextDeadline() time.Time {
@@ -234,11 +289,10 @@ func (p *Packet) handle(data []byte, addr net.Addr) {
 		p.storage.Store(addrstr, out)
 		go p.proxy(out, addr)
 	}
-	if _, err = out.Write(data); err != nil {
-		log.Println(err)
-		return
+	if _, err = out.Write(data); err == nil {
+		err = out.SetReadDeadline(p.nextDeadline())
 	}
-	if err = out.SetReadDeadline(p.nextDeadline()); err != nil {
+	if err != nil && !isErrNetClosing(err) {
 		log.Println(err)
 	}
 }
@@ -269,7 +323,7 @@ func (p *Packet) proxy(out net.Conn, addr net.Addr) {
 			}
 		}
 		if err != nil {
-			if !os.IsTimeout(err) {
+			if !isTimeout(err) {
 				log.Println(err)
 			}
 			break
@@ -278,4 +332,15 @@ func (p *Packet) proxy(out net.Conn, addr net.Addr) {
 			log.Println(err)
 		}
 	}
+}
+
+func isTimeout(err error) bool {
+	netErr, ok := err.(net.Error)
+	return ok && netErr.Timeout()
+}
+
+func isErrNetClosing(err error) bool {
+	// https://github.com/golang/go/issues/4373
+	// https://golang.org/src/internal/poll/fd.go?h=ErrNetClosing
+	return strings.HasSuffix(err.Error(), "use of closed network connection")
 }
