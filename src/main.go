d0af4c2ef011e02f50adab0d9eab950281eecf84
lucky <>
2020-10-12 20:37:06 +0300

    add FD_CLOEXEC to fds

--- src/main.go
+++ src/main.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	Version = "0.1.5"
+	Version = "0.1.6"
 
 	FlagPlist       = "p"
 	FlagDestination = "d"
@@ -35,6 +35,7 @@ func start(name string, opts *Opts) error {
 		return err
 	}
 	for _, fd := range fds {
+		syscall.CloseOnExec(fd)
 		socketType, err := syscall.GetsockoptInt(
 			fd,
 			syscall.SOL_SOCKET,
